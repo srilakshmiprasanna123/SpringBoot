098c54e68bb189c38e4142ccb2e04679d752b324
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-12 04:48:42 +0530

    added use get request method and pass name as query parameter

---
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringbootdemoApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Spring boot concept");
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+}
